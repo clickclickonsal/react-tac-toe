cd0a774071e449b9075484621bc7dcf52e5b3e4e
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-04 14:35:59 -0500

    added a menu for displaying who's turn it is

--- js/app.js
+++ js/app.js
@@ -13,16 +13,17 @@ var Game = React.createClass({
 				'', '', ''
 			],
 			// O Goes first
-			playerTurn: 'O'
+			turn: 'O'
 		}
 	},
 	render: function(){
 		return (
 			<div id="game">
+				<Menu turn={this.state.turn} />
 				{ this.state.tiles.map(function(tile, position){
 					console.log(position)
 					return (
-						<Tile status={tile} key={position} turn={this.state.playerTurn} />
+						<Tile status={tile} key={position} turn={this.state.turn} />
 					)
 				}, this)}
 			</div>
@@ -34,6 +35,14 @@ var Tile = React.createClass({
 	render: function(){
 		return (
 			<div className={this.props.status === '' ? 'tile' : 'tile status-' + this.props.status}>{this.props.status}</div>
+		)
+	}
+});
+
+var Menu = React.createClass({
+	render: function() {
+		return (
+			<h2>"Player {this.props.turn}'s Turn"</h2>
 		)
 	}
 });
